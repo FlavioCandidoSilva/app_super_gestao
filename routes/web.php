d1118e694dae4d1a0f24b9b9037cf3c22cffc3fc
FlavioCandidoSilva <62729183+FlavioCandidoSilva@users.noreply.github.com>
2021-11-30 12:54:00 -0300

    master

--- routes/web.php
+++ routes/web.php
@@ -21,9 +21,24 @@ Route::get('/', function () {
 */
 
 Route::get('/','PrincipalController@principal');
-
 Route::get('/sobre-nos','SobreNosController@sobreNos');
+Route::get('/contato','ContatoController@contato'); // nome, categoria, assunto, mensagem
+Route::get('/login', function(){return 'login';});
 
-Route::get('/contato','ContatoController@contato');
 
+Route::prefix('/app')->group(function(){
+    Route::get('/clientes',function(){return 'cliente';});
+    Route::get('/fornecedores',function(){return 'fornecedores';});
+    Route::get('/produtos',function(){return 'produtos';});
+});
 
+/*
+Route::get('/contato/{nome}/{categoria_id}', 
+function(
+    string $nome = 'Desconhecido', 
+    int $categoria_id = 1 // 1 - 'informação'
+    ){
+        echo "estamos aqui: $nome-$categoria_id";
+    }
+)->where('categoria_id','[0-9]+')->where('nome','[A-Za-z]+');
+*/
