2e8bfa32b1aa1809679af1b26b461749ff8dfb86
FlavioCandidoSilva <62729183+FlavioCandidoSilva@users.noreply.github.com>
2021-12-20 15:47:36 -0300

    maste

---
+++ database/migrations/2021_12_20_181504_create_pruduto_detalhes_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePrudutoDetalhesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('produtos', function (Blueprint $table) {             
+            $table->id();             
+            $table->string('nome', 100);   
+            $table->foreignId('produto_id')->references('id')->on('produtos');   
+            $table->unique('produto_id');     
+            $table->string('nome', 100);              
+            $table->text('descricao')->nullable();
+            $table->integer('peso')->nullable();
+            $table->float('preco_venda', 8,2)->default(0.01);
+            $table->integer('estoque_minimo')->default(1);
+            $table->integer('estoque_maximo')->default(1);
+            $table->timestamps(); 
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pruduto_detalhes');
+    }
+}
